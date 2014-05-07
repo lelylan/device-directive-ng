fe88b3de4610710dec1f7620ec77fd1be3dc4984
Andrea Reginato <andrea.reginato@gmail.com>
2014-05-07 17:10:36 +0200

    Added the first tests using protractor to check the widget behaviour

--- test/integrations/device.js
+++ test/integrations/device.js
@@ -1,6 +1,67 @@
 describe('<device>', function() {
-  it('should see the widget', function() {
-    browser.get('/');
-    expect(element(by.css('.ly-header .ly-name')).getText()).toEqual('Closet dimmer');
+
+  beforeEach(function() {
+    browser.get('http://localhost:9000');
+  });
+
+
+  describe('header', function() {
+
+    it('shows the device name', function() {
+      var name = $('.ly-header .ly-name').getText();
+      expect(name).toEqual('Closet dimmer');
+    });
+
+    it('does not show the settings', function() {
+      expect($('.ly-settings').isPresent()).toBe(false);
+    });
+  });
+
+  describe('settings', function() {
+
+    describe('when open', function() {
+
+      beforeEach(function() {
+        $('.ly-menu .ly-menu-settings').click();
+      });
+
+      it('shows the settings window', function() {
+        expect($('.ly-settings-device').isPresent()).toBe(true);
+      });
+
+      it('shows the settings info', function() {
+        expect($('.ly-settings .ly-form .ly-id').getAttribute('value')).toBe('1');
+        expect($('.ly-settings .ly-form .ly-secret').getAttribute('value')).toBe('secret');
+        expect($('.ly-settings .ly-form .ly-type').getText()).toBe('Dimmer');
+        expect($('.ly-settings .ly-form .ly-name').getAttribute('value')).toBe('Closet dimmer');
+        expect($('.ly-settings .ly-form .ly-physical').getAttribute('value')).toBe('https://node.lelylan.com/mqtt/devices/1');
+      });
+
+      describe('when closes', function() {
+
+        describe('when clicks on the close button', function() {
+
+          beforeEach(function() {
+            $('.ly-settings .ly-close').click();
+          });
+
+          it('closes the settings window', function() {
+            expect($('.ly-settings-device').isPresent()).toBe(false);
+          });
+        });
+
+        // TODO this test should work but it does not
+        //describe('when clicks on the modal backdrop', function() {
+
+          //beforeEach(function() {
+            //$('.ly-settings .ly-modal-backdrop').click();
+          //});
+
+          //it('closes the settings window', function() {
+            //expect($('.ly-settings').isPresent()).toBe(false);
+          //});
+        //});
+      });
+    });
   });
 });
