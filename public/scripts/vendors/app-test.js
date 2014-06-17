22c282aa82b6c2dcfb0be4b280e2afdefc5fd138
Andrea Reginato <andrea.reginato@gmail.com>
2014-06-17 09:45:23 +0200

    Added style to get the best view for the widget

--- public/scripts/vendors/app-test.js
+++ public/scripts/vendors/app-test.js
@@ -4,7 +4,14 @@ var app = angular.module('app', ['lelylan.directives.device', 'ngMockE2E']);
 app.run(function($httpBackend, $timeout, Profile) {
   Profile.set({id: '1'});
 
+  jasmine.getFixtures().fixturesPath = 'public/scripts/fixtures';
+
+  device   = angular.copy(JSON.parse(readFixtures('device.json')));
+  type     = JSON.parse(readFixtures('type.json'));
+  privates = JSON.parse(readFixtures('privates.json'));
+
   $httpBackend.when('GET', /\/templates\//).passThrough();
+
   $httpBackend.whenGET('http://api.lelylan.com/devices/1').respond(device);
   $httpBackend.whenPUT('http://api.lelylan.com/devices/1')
     .respond(function(method, url, data, headers) { return [200, updateDevice(data), {}]; });
@@ -13,6 +20,14 @@ app.run(function($httpBackend, $timeout, Profile) {
   $httpBackend.whenDELETE('http://api.lelylan.com/devices/1').respond(device);
   $httpBackend.whenGET('http://api.lelylan.com/types/1').respond(type);
   $httpBackend.whenGET('http://api.lelylan.com/devices/1/privates').respond(privates);
+
+  $httpBackend.whenGET('http://api.lelylan.com/devices/2').respond(device);
+  $httpBackend.whenPUT('http://api.lelylan.com/devices/2')
+    .respond(function(method, url, data, headers) { return [200, updateDevice(data), {}]; });
+  $httpBackend.whenPUT('http://api.lelylan.com/devices/2/properties')
+    .respond(function(method, url, data, headers) { return [200,  updateDeviceProperties(data), {}]; });
+  $httpBackend.whenDELETE('http://api.lelylan.com/devices/2').respond(device);
+
 
   var updateDevice = function(data) {
     data = angular.fromJson(data);
@@ -31,164 +46,3 @@ app.run(function($httpBackend, $timeout, Profile) {
     return device;
   }
 });
-
-device = {
-  "id": "1",
-  "uri": "http://www.example.com/devices/1",
-  "name": "Closet dimmer",
-  "categories": ["lights"],
-  "pending": false,
-  "activated": true,
-  "type": {
-    "id": "1",
-    "uri": "https://type.lelylan.com/types/dimmer"
-  },
-  "properties": [{
-    "id": "1",
-    "uri": "https://type.lelylan.com/properties/status",
-    "value": "off",
-    "expected": "off",
-    "pending": false,
-    "accepted": null
-  }, {
-    "id": "2",
-    "uri": "https://type.lelylan.com/properties/intensity",
-    "expected": "0",
-    "value": "0",
-    "pending": false,
-    "accepted": null
-  }],
-  "physical": {
-    "uri": "https://node.lelylan.com/mqtt/devices/1"
-  },
-  "maker": {
-    "id": "1",
-    "uri": "http://api.lelylan.com/people/1"
-  },
-  "owner": {
-    "id": "2",
-    "uri": "http://api.lelylan.com/people/2"
-  },
-  "created_at": "2011-06-20T15:54:42Z",
-  "updated_at": "2011-04-23T15:55:31Z",
-  "updated_from": "you"
-}
-
-privates = {
-  "uri": "http://api.lelylan.com/devices/5042344b95fc441000000001",
-  "id": "1",
-  "name": "Closet dimmer",
-  "secret": "secret",
-  "activation_code": "activation_code"
-}
-
-type = {
-    "id": "1",
-    "uri": "http://www.example.com/types/50042612d033a9b4ac0007fa",
-    "name": "Dimmer",
-    "description": "Dimmer description",
-    "categories": ["lights"],
-    "owner": {
-      "id": "1",
-      "uri": "http://api.lelylan.com/people/1"
-    },
-    "created_at": "2012-07-16T14:32:50Z",
-    "updated_at": "2012-07-16T14:32:50Z",
-    "properties": [{
-      "id": "1",
-      "uri": "http://www.example.com/properties/status",
-      "name": "Status",
-      "type": "text",
-      "default": "0",
-      "accepted": { "on": "On", "off": "Off" },
-      "created_at": "2012-07-16T14:32:50Z",
-      "updated_at": "2012-07-16T14:32:50Z"
-    },
-    {
-      "uri": "http://www.example.com/properties/intensity",
-      "id": "2",
-      "name": "Intensity",
-      "type": "range",
-      "range": { "min": "0", "max": "100", "step": "1" },
-      "default": "0",
-      "created_at": "2012-07-16T14:32:50Z",
-      "updated_at": "2012-07-16T14:32:50Z"
-    }],
-    "functions": [{
-        "uri": "http://www.example.com/functions/50042612d033a9b4ac0007f0",
-        "id": "1",
-        "name": "Turn on",
-        "created_at": "2012-07-16T14:32:50Z",
-        "updated_at": "2012-07-16T14:32:50Z",
-        "properties": [{
-          "id": "1",
-          "uri": "http://www.example.com/properties/status",
-          "expected": "on"
-        }]
-    },
-    {
-        "uri": "http://www.example.com/functions/50042612d033a9b4ac0007f2",
-        "id": "2",
-        "name": "Turn off",
-        "created_at": "2012-07-16T14:32:50Z",
-        "updated_at": "2012-07-16T14:32:50Z",
-        "properties": [{
-          "id": "1",
-          "uri": "http://www.example.com/properties/status",
-          "expected": "off"
-        }]
-    },
-    {
-        "uri": "http://www.example.com/functions/50042612d033a9b4ac0007f5",
-        "id": "3",
-        "name": "Set intensity",
-        "created_at": "2012-07-16T14:32:50Z",
-        "updated_at": "2012-07-16T14:32:50Z",
-        "properties": [{
-          "id": "1",
-          "uri": "http://www.example.com/properties/status",
-          "expected": "on"
-        },
-        {
-          "id": "2",
-          "uri": "http://www.example.com/properties/intensity",
-          "expected": null
-        }]
-    }],
-    "statuses": [{
-        "uri": "http://www.example.com/statuses/50042612d033a9b4ac0007f8",
-        "id": "50042612d033a9b4ac0007f8",
-        "name": "On",
-        "created_at": "2012-07-16T14:32:50Z",
-        "updated_at": "2012-07-16T14:32:50Z",
-        "function": {
-          "id": "2",
-          "uri": "http://www.example.com/functions/turn-off"
-        },
-        "properties": [{
-          "id": "1",
-          "uri": "http://www.example.com/properties/status",
-          "pending": false,
-          "values": ["on"],
-          "ranges": []
-        }]
-      }, {
-        "uri": "http://www.example.com/statuses/50042612d033a9b4ac0007f8",
-        "id": "50042612d033a9b4ac0007f8",
-        "name": "Off",
-        "created_at": "2012-07-16T14:32:50Z",
-        "updated_at": "2012-07-16T14:32:50Z",
-        "function": {
-          "id": "1",
-          "uri": "http://www.example.com/functions/turn-on"
-        },
-        "properties": [{
-          "id": "1",
-          "uri": "http://www.example.com/properties/status",
-          "pending": false,
-          "values": ["off"],
-          "ranges": []
-        }]
-    }]
-}
-
